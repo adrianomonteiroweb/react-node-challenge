da5b107fd577d347486393c41695f260d295e34b
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-08-31 10:38:41 -0300

    NEW: gettings treatments methos and tests settings

--- backend/src/routers/treatments.routes.js
+++ backend/src/routers/treatments.routes.js
@@ -1,6 +1,9 @@
 const {
   addTreatmentController,
   updateTreatmentController,
+  getTreatmentsController,
+  getTreatmentByIDController,
+  getTreatmentByPatientController,
 } = require('../database/controllers/treatments.controller');
 
 const treatmentRoutes = require('express').Router();
@@ -8,5 +11,14 @@ const treatmentRoutes = require('express').Router();
 treatmentRoutes.post('/treatment', addTreatmentController);
 
 treatmentRoutes.put('/treatment/:id', updateTreatmentController);
+
+treatmentRoutes.get('/treatment', getTreatmentsController);
+
+treatmentRoutes.get('/treatment/:id', getTreatmentByIDController);
+
+treatmentRoutes.get(
+  '/treatment/patient/:patientid',
+  getTreatmentByPatientController
+);
 
 module.exports = treatmentRoutes;
