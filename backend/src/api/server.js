7470abd47c6664347b203c5613df9036eefc33e6
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-09-01 16:24:59 -0300

    FIX: settings in the project structure for workspaces

--- backend/src/api/server.js
+++ backend/src/api/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const HOST = process.env.HOST || 'localhost';
+
 const DEV =
   process.env.DEV === 'true'
     ? `Listenning on http://${HOST}:${PORT}`
