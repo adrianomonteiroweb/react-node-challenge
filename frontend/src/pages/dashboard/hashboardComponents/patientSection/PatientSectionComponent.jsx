30d460960d7ba8aaac90846498032216a6bd2750
adriano <adrianomonteiroweb@gmail.com>
2022-09-12 19:28:29 -0300

    NEW: dashboard settings with view more link for treatments

--- frontend/src/pages/dashboard/hashboardComponents/patientSection/PatientSectionComponent.jsx
+++ frontend/src/pages/dashboard/hashboardComponents/patientSection/PatientSectionComponent.jsx
@@ -1,11 +1,21 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function PatientSectionComponent({
-  patient: { firstName, lastName },
+  patient: { firstName, lastName, email, number },
 }) {
   return (
     <div className='carousel-card'>
       <h3 className='patient-name'>{`${firstName} ${lastName}`}</h3>
+      <span>
+        Email: <p>{email}</p>
+      </span>
+      <span>
+        Telefone: <p>{number}</p>
+      </span>
+      <Link className='view-more' to='/treatments'>
+        <span>View More</span>
+      </Link>
     </div>
   );
 }
